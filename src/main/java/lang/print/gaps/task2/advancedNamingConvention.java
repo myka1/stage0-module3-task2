4c164377d0facbe9c71889ca1d2ab6ddc81abd93
myka1 <msmykolas@gmail.com>
2022-06-17 15:37:05 +0300

    Update advancedNamingConvention.java

--- src/main/java/lang/print/gaps/task2/advancedNamingConvention.java
+++ src/main/java/lang/print/gaps/task2/advancedNamingConvention.java
@@ -1,19 +1,16 @@
 package lang.print.gaps.task2;
 
-public class AdvancedNamingConvention {
+public class advancedNamingConvention {
     private static final int ADULT_AGE = 18;
 
     private int age;
     private int phoneNumber;
 
-    void callToFriend() {
+    public static void callToFriend() {
         //some code
     }
 
-    void callByNumber(int number) {
+    public static void callByNumber(int number) {
         //some code
-    }
-    public static void main(String[] args) {
-
     }
 }
